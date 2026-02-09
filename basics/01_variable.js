c7f8f0a10954acbd1c9ff9ce23a02d51ba63984d
priyanshu kumar <137908281+xpriyanshuk@users.noreply.github.com>
2026-02-09 03:16:29 +0000

    add comment

--- basics/01_variable.js
+++ basics/01_variable.js
@@ -3,6 +3,20 @@ let accountEmail="xpriyanshuk@gmail.com"
 var accountPassword = "18188"
 accountCity="jaipur"
 let accountState;
+//prefer not to use var beacuse of issue in block scope and functional scope
+
+
+if (true) {
+  var x = 10;
+}
+console.log(x); // 10 😬 (leaks outside)
+
+if (true) {
+  let y = 20;
+}
+console.log(y); // ❌ ReferenceError
+
+
 //accountId = 19 not allowed beacause we are assinging in const variable
 accountEmail="pk@gmail.com"
 accountPassword="6789"
