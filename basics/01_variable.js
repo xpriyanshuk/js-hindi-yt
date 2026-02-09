329ff81f33cb3d9da74024d266942aecccec738c
priyanshu kumar <137908281+xpriyanshuk@users.noreply.github.com>
2026-02-09 02:51:57 +0000

    variable

---
+++ basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 15526
+let accountEmail="xpriyanshuk@gmail.com"
+var accountPassword = "18188"
+accountCity="jaipur"
+let accountState;
+//accountId = 19 not allowed beacause we are assinging in const variable
+accountEmail="pk@gmail.com"
+accountPassword="6789"
+accountCity="delhi"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
